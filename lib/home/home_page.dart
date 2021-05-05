264d27cb3bae7ebb08e46dafa4a43e074b111df2
Mateus Zilli Polli <mateus_zillip@hotmail.com>
2021-05-05 15:29:03 -0300

    :rocket:

---
+++ lib/home/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'package:DevQuiz/home/widgets/app_bar/app_bar_widget.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBarWidget(),
+      )
+    );
+  }
+}
---
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'package:DevQuiz/core/app_colors.dart';
+import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/home/widgets/chart/chart_widget.dart';
+
+class ScoreCardWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      child: Container(
+        height: 136,
+        decoration: BoxDecoration(
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(15)
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 1,
+              child: ChartWidget(),
+            ),
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 24),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Let's start", style: AppTextStyles.heading),
+                    Text("Complete challenges and advance knowledge", style: AppTextStyles.body)
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
